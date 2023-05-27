901387fccdb41672a9269094a54bce0deb107156
Akshay Neje <akshayneje2823@gmail.com>
2023-05-27 16:00:52 +0530

    done upto bcrypt

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
 import router from  './routes/authRoute.js';
-import { connectDB } from './config/db.js';
+// import { connectDB } from './config/db.js';
 import { errorHandler } from './middleware/error.js';
 
 // config file
@@ -15,7 +16,16 @@ app.use(express.json());
 app.use(cors());
 
 // connect DB
-connectDB()
+
+export const connectDB = async () => {
+    await mongoose.connect("mongodb://0.0.0.0:27017/userAuth", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(res => {
+        console.log("connected to DB")
+    })
+    .catch(err => console.log(err.name))
+}
 
 
 // router
@@ -24,12 +34,15 @@ app.use('/api/auth',router)
 app.use(errorHandler)
 
 const server = app.listen(process.env.PORT, () => {
+    connectDB()
+
     console.log("server is running " + process.env.PORT)
 })
 
 
 //any error occur
 process.on("unhandledRejection",(err,promise)=>{
+
     console.log(`Logged Error: ${err}`);
     server.close(()=>process.exit(1))
 })
